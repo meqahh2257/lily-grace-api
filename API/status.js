efa984f2e3d1b96fff1656f9ca7b3cead5f9dbab
meqahh2257 <meqahh2257@gmail.com>
2025-12-14 14:58:31 -0700

    Delete API directory

--- API/status.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (req, res) => {
-  res.status(200).json({
-    vercelEnv: process.env.VERCEL_ENV || null,          // "production" or "preview"
-    hasStorefrontToken: !!process.env.SHOPIFY_STOREFRONT_TOKEN
-  });
-};
